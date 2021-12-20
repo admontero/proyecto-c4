aa70e81d76f6b455366a3f377a12f9927e508438
admontero <adavidmontero@outlook.com>
2021-12-19 19:55:28 -0500

    Arreglo

--- src/index.js
+++ src/index.js
@@ -23,28 +23,11 @@ const server = new ApolloServer({
     resolvers: resolvers
 });
 
-const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME;
-const MONGO_PORT = process.env.MONGO_PORT;
-const MONGO_DB = process.env.MONGO_DB;
-
-const DB_URL = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
-
-if (process.env.NODE_ENV === 'production') {
-    mongoose.connect(DB_URL, async () => {
-        console.log("conexión exitosa a la base de datos");
-        app.listen(process.env.PORT || 5000, async () => {
-            await server.start();
-            server.applyMiddleware({ app });
-            console.log("Corriendo en el puerto 5000...");
-        });
+mongoose.connect(process.env.DB_MONGO, async () => {
+    console.log("conexión exitosa a la base de datos");
+    app.listen(process.env.PORT || 5000, async () => {
+        await server.start();
+        server.applyMiddleware({ app });
+        console.log("Corriendo en el puerto 5000...");
     });
-} else {
-    mongoose.connect(process.env.DB_MONGO, async () => {
-        console.log("conexión exitosa a la base de datos");
-        app.listen(process.env.PORT || 5000, async () => {
-            await server.start();
-            server.applyMiddleware({ app });
-            console.log("Corriendo en el puerto 5000...");
-        });
-    });
-}
+});
